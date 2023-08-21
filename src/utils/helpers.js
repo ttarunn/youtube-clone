9bc259374d572446c05a4623ec45dead042fb632
Tarun <ttarunn0709@gmail.com>
2023-08-21 18:30:09 +0530

    redirect

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,8 +1,15 @@
-// import { YOUTUBE_SEARCH_API } from "./constants";
+import { YOUTUBE_SEARCH_API } from "./constants";
 
-// export const getSearchSuggetsions = async () => {
-//     const data = await fetch(YOUTUBE_SEARCH_API + "searchText");
-//     const json = await data.json();
-//     // setSerachData(json[1])
-//     console.log(json[1])
-//   }
+export const getSearchSuggetsions = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_API + "searchText", {
+        method:"GET",
+        crossorigin: true,    
+        mode: 'no-cors', 
+        headers:{
+            "Access-Control-Allow-Origin":"*"
+        }
+    });
+    const json = await data.json();
+    // setSerachData(json[1])
+    console.log(json[1])
+  }
